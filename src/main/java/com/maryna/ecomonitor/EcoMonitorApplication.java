9b9b2fefc21adaba8e7300c1c3476e40c1bf6089
rosadamaryna <rosada.maryna@student.uzhnu.edu.ua>
2026-02-18 22:54:46 +0200

    feat: initial project structure and technical specifications

---
+++ src/main/java/com/maryna/ecomonitor/EcoMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.maryna.ecomonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryna/ecomonitor/EcoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryna.ecomonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
