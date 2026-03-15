9a31a45b6f30af66714b06ccbf3e8ee50ffa5878
rosadamaryna <rosada.maryna@student.uzhnu.edu.ua>
2026-03-15 11:20:00 +0200

    Initial commit: Core layout from Spring Initializr with Web, JPA, and PostgreSQL dependencies

---
+++ ecomonitor/src/main/java/ua/edu/uzhnu/ecomonitor/EcomonitorApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.uzhnu.ecomonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomonitorApplication.class, args);
+	}
+
+}
---
+++ ecomonitor/src/test/java/ua/edu/uzhnu/ecomonitor/EcomonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.uzhnu.ecomonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
